ccc08b99f9260ea497fe9defdd47307f49dab198
Faranak <faranak.rahimi@liip.ch>
2024-04-25 10:55:11 +0200

    add some div

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inventory: 100,
-            // solution
-            onSale: true
-            // solution
+            onSale: true,
+            details:['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:2234, color:'green'},
+                {id:2235, color:'blue'},
+
+            ],
+            sizes:['xs','s','m','l','xl'],
         }
     }
 })
